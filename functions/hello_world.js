4ec78c1ee14cc4fe9cb56da4dce191c098a144ae
dmitryame <dmitry@custommode.com>
2017-12-31 16:15:46 -0500

    bootstrap serverless

---
+++ functions/hello_world.js
@@ -0,0 +1,28 @@
+export  async function main(event, context, callback) {
+  // parameters passed into the call
+  // console.log("debug:", 1)
+
+  const message = "Hello world!"
+
+  var response
+
+  try {
+
+    response = {
+      statusCode: 200,
+      body: JSON.stringify(message)
+    }
+    // console.log("debug:", 4)
+
+    callback(null, response)
+
+  } catch (error) {
+    console.log("error from calling API:", error.response.data)
+
+    response = {
+      statusCode: 500,
+      body: JSON.stringify(error.response.data)
+    }
+    callback(null, response)
+  }
+}
