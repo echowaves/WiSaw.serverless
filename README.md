d7c22d734913b8fec4642b115428132956a6f124
dmitryame <dmitry@custommode.com>
2018-01-07 23:38:54 -0500

    migrations config

--- README.md
+++ README.md
@@ -12,3 +12,12 @@ last argument is stage (requred), either dev or prod
 
 the test scripts are always executing on test stage and never on prod, if tests pass on "test -- it will work on prod for sure
   npm run test
+
+
+to run migrations
+  npm run migrate test
+  npm run migrate prod
+
+to undo one migration  
+  npm run migrate-undo test
+  npm run migrate-undo prod
---
+++ config/config.js
@@ -0,0 +1,32 @@
+const argv = require('yargs').argv
+
+const env = argv.env || "test";
+
+console.log("env", env);
+const config = require('../.env.'+ env).config();
+
+
+
+module.exports = {
+  dev: {
+    username: config.DB_USERNAME,
+    password: config.DB_PASSWORD,
+    database: config.DB_DATABASE,
+    host: config.DB_HOST,
+    dialect: config.DB_DIALECT
+  },
+  test: {
+    username: config.DB_USERNAME,
+    password: config.DB_PASSWORD,
+    database: config.DB_DATABASE,
+    host: config.DB_HOST,
+    dialect: config.DB_DIALECT
+  },
+  prod: {
+    username: config.DB_USERNAME,
+    password: config.DB_PASSWORD,
+    database: config.DB_DATABASE,
+    host: config.DB_HOST,
+    dialect: config.DB_DIALECT
+  }
+};
