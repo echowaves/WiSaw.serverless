27bcad12bc19f5bca1866108daa0b1939faeff57
dmitryame <dmitry@custommode.com>
2018-01-10 16:15:34 -0500

    config improvements, bootstrapping contactForm controller

--- config/config.js
+++ config/config.js
@@ -2,30 +2,32 @@ const argv = require('yargs').argv
 
 const env = argv.env || "test";// default to test stage
 
-const config = require('../.env.'+ env).config();
+const dev_config = require('../.env.sample').config();
+const test_config = require('../.env.test').config();
+const prod_config = require('../.env.prod').config();
 
 
 
 module.exports = {
   dev: {
-    username: config.DB_USERNAME,
-    password: config.DB_PASSWORD,
-    database: config.DB_DATABASE,
-    host: config.DB_HOST,
-    dialect: config.DB_DIALECT
+    username: dev_config.DB_USERNAME,
+    password: dev_config.DB_PASSWORD,
+    database: dev_config.DB_DATABASE,
+    host: dev_config.DB_HOST,
+    dialect: dev_config.DB_DIALECT
   },
   test: {
-    username: config.DB_USERNAME,
-    password: config.DB_PASSWORD,
-    database: config.DB_DATABASE,
-    host: config.DB_HOST,
-    dialect: config.DB_DIALECT
+    username: test_config.DB_USERNAME,
+    password: test_config.DB_PASSWORD,
+    database: test_config.DB_DATABASE,
+    host: test_config.DB_HOST,
+    dialect: test_config.DB_DIALECT
   },
   prod: {
-    username: config.DB_USERNAME,
-    password: config.DB_PASSWORD,
-    database: config.DB_DATABASE,
-    host: config.DB_HOST,
-    dialect: config.DB_DIALECT
+    username: prod_config.DB_USERNAME,
+    password: prod_config.DB_PASSWORD,
+    database: prod_config.DB_DATABASE,
+    host: prod_config.DB_HOST,
+    dialect: prod_config.DB_DIALECT
   }
 };
