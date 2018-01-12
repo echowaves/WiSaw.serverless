7ab2be419731726d3026e9fdca929bfb8f143acc
dmitryame <dmitry@custommode.com>
2018-01-11 19:11:56 -0500

    attempt to implement serving binary image data, lambda is not ready yet

---
+++ api/src/controllers/photos/full.js
@@ -0,0 +1,53 @@
+import Photo from '../../models/photo'
+
+import moment from 'moment'
+import Sequelize from 'sequelize'
+
+export  async function main(event, context, callback) {
+  //Instruct the lambda to exit immediately
+  //and not wait for node event loop to be empty.
+  context.callbackWaitsForEmptyEventLoop = false
+  const id = event.pathParameters.id
+
+  // retrieve photos
+  let photo
+  try {
+    photo = await Photo.findOne({
+      where: { id },
+      attributes:  { exclude: ["thumbNail"] }
+    })
+    if(!photo) {
+      const response = {
+        statusCode: 404,
+        body: JSON.stringify({ error: 'not found' })
+      }
+      callback(null, response)
+      return
+    }
+  } catch(err) {
+    console.log("Unable to retrieve a Full Photo", err)
+
+    const response = {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Unable to retrieve a Full Photo'})
+    }
+    callback(null, response)
+    return
+  }
+
+  // Resond to request indicating the photo was created
+  const response = {
+    statusCode: 200,
+    body: photo.imageData.toString('base64'),
+    isBase64Encoded: true,
+    headers: {
+      'Content-Type': 'image/png'
+    }
+  }
+  callback(null, response)
+}
+
+
+
+// https://github.com/serverless/serverless/issues/2797
+// https://forum.serverless.com/t/returning-binary-data-jpg-from-lambda-via-api-gateway/796/25
---
+++ api/src/controllers/photos/thumb.js
@@ -0,0 +1,47 @@
+import Photo from '../../models/photo'
+
+import moment from 'moment'
+import Sequelize from 'sequelize'
+
+export  async function main(event, context, callback) {
+  //Instruct the lambda to exit immediately
+  //and not wait for node event loop to be empty.
+  context.callbackWaitsForEmptyEventLoop = false
+  const id = event.pathParameters.id
+
+  // retrieve photos
+  let photo
+  try {
+    photo = await Photo.findOne({
+      where: { id },
+      attributes:  { exclude: ["imageData"] }
+    })
+    if(!photo) {
+      const response = {
+        statusCode: 404,
+        body: JSON.stringify({ error: 'not found' })
+      }
+      callback(null, response)
+      return
+    }
+  } catch(err) {
+    console.log("Unable to retrieve a Thumb of a Photo", err)
+
+    const response = {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Unable to retrieve a Thumb of a Photo'})
+    }
+    callback(null, response)
+    return
+  }
+  // Resond to request indicating the photo was created
+  const response = {
+    statusCode: 200,
+    headers: {
+                      'Content-Type': 'image/png'
+                  },
+    body: photo.thumbNail.toString('base64'),
+    isBase64Encoded: true
+  }
+  callback(null, response)
+}
