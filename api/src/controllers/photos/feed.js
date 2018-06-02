23ae89c9cbdf7dfea503722f45cf5d8bca55e988
dmitryame <dmitry@custommode.com>
2018-06-01 21:15:25 -0400

    upgrading to node8

--- api/src/controllers/photos/feed.js
+++ api/src/controllers/photos/feed.js
@@ -82,3 +82,92 @@ export async function main(event, context, callback) {
   callback(null, response)
   return true
 }
+
+
+// eslint-disable-next-line import/prefer-default-export
+export async function byDate(event, context, callback) {
+  // Instruct the lambda to exit immediately
+  // and not wait for node event loop to be empty.
+  context.callbackWaitsForEmptyEventLoop = false // eslint-disable-line no-param-reassign
+
+  const data = JSON.parse(event.body)
+  console.log({ data })
+
+  const location = data ? data.location : null
+  const date = data ? data.date : null
+  if (!data || !location || !date) {
+    console.log('setting status to 400')
+    const response = {
+      statusCode: 400,
+      body: JSON.stringify({ error: 'parameters missing' }),
+    }
+    callback(null, response)
+    return false
+  }
+
+  console.log('location:', location)
+  console.log('date:', date)
+
+  let limit = data ? data.limit : null
+  let offset = data ? data.offset : null
+  if (!limit) {
+    limit = 100
+  }
+  if (!offset) {
+    offset = 0
+  }
+
+  const lat = location.coordinates[0]
+  const lng = location.coordinates[1]
+
+  const point = Sequelize.fn('ST_MakePoint', lat, lng)
+
+
+  // retrieve photos
+  let photos
+  try {
+    photos = await Photo.findAll({
+      where: {
+        createdAt: {
+          $lt: new Date(date),
+          $gt: new Date(new Date(date) - (24 * 60 * 60 * 1000)),
+        },
+      },
+      attributes: {
+        include: [
+          [Sequelize.fn('ST_Distance', point, Sequelize.col('location')), 'distance'],
+        ],
+      },
+      order: Sequelize.col('distance'),
+      limit,
+      offset,
+    })
+    console.log('retrived photos:', photos.length)
+    // add img_url and thumb_url properties
+    // photos = dbPhotos.map((dbPhoto) => {
+    //   const photo = dbPhoto
+    //   photo.dataValues.img_url = `https://s3.amazonaws.com/${process.env.IMAGE_BUCKET}/${photo.id}`
+    //   photo.dataValues.thumb_url = `https://s3.amazonaws.com/${process.env.IMAGE_BUCKET}/${photo.id}-thumb`
+    //   // console.log({ photo })
+    //   return photo
+    // })
+    // console.log({ photos })
+  } catch (err) {
+    console.log('Unable to retrieve Photos feed', err)
+    const response = {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Unable to retrieve Photos feed' }),
+    }
+    callback(null, response)
+    return false
+  }
+
+
+  // Resond to request indicating the photo feed was created
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({ status: 'success', photos }),
+  }
+  callback(null, response)
+  return true
+}
