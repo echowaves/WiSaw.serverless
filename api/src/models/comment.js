aa5847e34fec96cec9f91ba2cb4e8b46373368a0
dmitryame <dmitry@custommode.com>
2019-01-24 19:06:06 -0500

    added Comment model

---
+++ api/src/models/comment.js
@@ -0,0 +1,44 @@
+import Sequelize from 'sequelize'
+import { sequelize } from '../../../config/consts'
+
+const Comment = sequelize.define('Comment', {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: Sequelize.INTEGER,
+  },
+  photoId: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    defaultValue: 0,
+  },
+  uuid: {
+    type: Sequelize.UUID,
+    allowNull: false,
+  },
+  comment: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  },
+  active: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false,
+    defaultValue: true,
+  },
+  deactivatedBy: {
+    type: Sequelize.UUID,
+    allowNull: true,
+    defaultValue: null,
+  },
+  createdAt: {
+    allowNull: false,
+    type: Sequelize.DATE,
+  },
+})
+
+// Adding a class level method
+
+// Adding an instance level method
+
+export default Comment
