e5bc8c30be0a212e72f379b8d582d9085c4d06ac
dmitryame <dmitry@custommode.com>
2019-01-24 19:02:43 -0500

    added Comments migration;

---
+++ migrations/20190124184800-create-comments.js
@@ -0,0 +1,41 @@
+module.exports = {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('Comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      photoId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      uuid: {
+        type: Sequelize.UUID,
+        allowNull: false,
+      },
+      comment: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      active: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
+      deactivatedBy: {
+        type: Sequelize.UUID,
+        allowNull: true,
+        defaultValue: null,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    })
+      .then(() => queryInterface.addIndex('Comments', ['photoId'])),
+  down: (queryInterface, Sequelize) => // eslint-disable-line no-unused-vars
+    queryInterface.dropTable('Comments'),
+}
