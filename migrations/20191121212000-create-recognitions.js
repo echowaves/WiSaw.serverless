220fa6fdd1eccc60f6628d85b112207d3e0b28ae
dmitryame <dmitry@custommode.com>
2019-11-26 21:41:51 -0500

    added full text index on comments

--- migrations/20191121212000-create-recognitions.js
+++ migrations/20191121212000-create-recognitions.js
@@ -26,8 +26,13 @@ module.exports = {
     })
       .then(() => queryInterface.addIndex('Recognitions', ['photoId']))
       .then(() =>
-        queryInterface.sequelize.query('CREATE INDEX idx_Recognitions_metaData_full_text ON "Recognitions" USING gist ((to_tsvector(\'English\', "metaData"::text)))')),
+        queryInterface.sequelize.query('CREATE INDEX idx_Recognitions_metaData_full_text ON "Recognitions" USING gist ((to_tsvector(\'English\', "metaData"::text)))'))
+      .then(() =>
+        queryInterface.sequelize.query('CREATE INDEX idx_Comments_metaData_full_text ON "Comments" USING gist ((to_tsvector(\'English\', "comment"::text)))')),
 
-  down: (queryInterface, Sequelize) => // eslint-disable-line no-unused-vars
-    queryInterface.dropTable('Recognitions'),
+  down: (queryInterface, Sequelize) => { // eslint-disable-line no-unused-vars
+    queryInterface.dropTable('Recognitions')
+    return queryInterface.sequelize.query('DROP INDEX idx_Comments_metaData_full_text')
+  },
+
 }
