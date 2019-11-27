7739db01bba2b6967ac9ffe7595642384d88a490
dmitryame <dmitry@custommode.com>
2019-11-26 20:34:49 -0500

    added full text search index

--- migrations/20191121212000-create-recognitions.js
+++ migrations/20191121212000-create-recognitions.js
@@ -25,7 +25,9 @@ module.exports = {
       },
     })
       .then(() => queryInterface.addIndex('Recognitions', ['photoId']))
-      .then(() => queryInterface.addIndex('Recognitions', ['metaData'])),
+      .then(() =>
+        queryInterface.sequelize.query('CREATE INDEX idx_Recognitions_metaData_full_text ON "Recognitions" USING gist ((to_tsvector(\'English\', "metaData"::text)))')),
+
   down: (queryInterface, Sequelize) => // eslint-disable-line no-unused-vars
     queryInterface.dropTable('Recognitions'),
 }
